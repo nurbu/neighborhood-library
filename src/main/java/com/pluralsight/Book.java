f06faa2b8cfc6a1b5190b69840cfaf8031868ce7
Nurbu <lamanurbu2@gmail.com>
2026-04-17 15:03:25 -0400

    Completed Getters, Setters and the toString for the Book object.

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -16,4 +16,48 @@ public class Book {
         this.checkedOutTo = checkedOutTo;
     }
 
+    public int getId() {
+        return id;
+    }
 
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public String getCheckedOutTo() {
+        return checkedOutTo;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCheckedOut(boolean checkedOut) {
+        isCheckedOut = checkedOut;
+    }
+
+    public void setCheckedOutTo(String checkedOutTo) {
+        this.checkedOutTo = checkedOutTo;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + " ISBN: " + isbn + " TITLE: " + title;
+    }
+}
