830f30373cf1e5bf0c9356e39aea8c379a6d65ff
Nurbu <lamanurbu2@gmail.com>
2026-04-17 14:58:44 -0400

    Finished variable declarations for Book class and added them to the constructor.

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -1,4 +1,19 @@
 package com.pluralsight;
 
 public class Book {
-}
+
+    private int id;
+    private String isbn;
+    private String title;
+    private boolean isCheckedOut;
+    private String checkedOutTo;
+
+    public Book(int id, String isbn, String title, boolean isCheckedOut, String checkedOutTo) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.isCheckedOut = isCheckedOut;
+        this.checkedOutTo = checkedOutTo;
+    }
+
+
